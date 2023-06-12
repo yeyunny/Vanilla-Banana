4b663b752dcd01074b53de8fbda96d487ea1f0bd
yeyun <hallo.yunye@gmail.com>
2023-06-13 00:40:46 +0900

    [MODIFY] greeting 코드 수정

--- js/greetings.js
+++ js/greetings.js
@@ -1,13 +1,14 @@
 const loginForm = document.querySelector("#login-form");
+const newLoginFom = document.createElement("form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_IDNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginForm.setAttribute("id", HIDDEN_IDNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -15,14 +16,13 @@ function onLoginSubmit(event) {
 
 function paintGreetings(username) {
   greeting.innerText = `${username}`;
-  greeting.style.display = "block";
-  // greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.setAttribute("id", HIDDEN_IDNAME);
 }
 
 const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 if (savedUserName === null) {
-  // loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.removeAttribute("id", HIDDEN_IDNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUserName);
